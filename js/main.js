22f9d9e9a4f889c3795db93e716c16e6afceacd7
LESEHY <tmdgus2496@gmail.com>
2023-02-27 17:36:28 +0900

    Update main.js

--- js/main.js
+++ js/main.js
@@ -1,11 +1,14 @@
 $(() => {
     console.log("로딩완");
 
-   let num = $(".num li");
-   
+    let num = $(".num");
 
-   
+    num.on('click','li',function(){
+        const text = $(this).text();
+        console.log(text);
+    })
 
     
+
 
 }); // JQB
