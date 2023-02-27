9ecc0a730d9069846c2b4e5a32a990cf0e8f2a37
LESEHY <tmdgus2496@gmail.com>
2023-02-27 21:09:51 +0900

    Update main.js

--- js/main.js
+++ js/main.js
@@ -4,6 +4,10 @@ $(() => {
     let num = $(".num");
     let firstPW = "0000";
     let inpw = "";
+    let impw = "";
+    let pw;
+    let changePW = "*253*"
+
     // 클릭했을 때 숫자인식
     num.on('click', 'li', function () {
         const text = $(this).text();
@@ -19,22 +23,39 @@ $(() => {
 
         let result = $(".result")
 
-        if (inpw === firstPW) {
-            result.html("비밀번호를 변경하세요.")
-            // 입력 초기화
-            inpw = "";
-            num.find("li").removeClass("active");
 
-        } else {
-            result.html("비밀번호가 일치하지 않습니다.")
+        if (inpw === firstPW || inpw === pw) {
             // 입력 초기화
             inpw = "";
             num.find("li").removeClass("active");
+            result.html("잠금해제")
+        } 
+        else if (inpw === changePW) {
+            result.html("새로운 비밀번호로 변경하시려면 *164*를 눌러주세요!");
+            inpw = "";
+            if (inpw === "*164*") {
+                firstPW = "";
+                inpw = "";
+                result.html("새로운 비밀번호를 입력하세요.");
+
+
+                // result.html("비밀번호가 변경되었습니다.");
+            }
+
+        } 
+        else {
+            // 입력 초기화
+            inpw = "";
+            num.find("li").removeClass("active");
+            result.html("비밀번호가 일치하지 않습니다.")
         }
+
 
         /* 위 코드에서 num.find("li").removeClass("active")는 입력 필드에서 선택한 숫자들의 활성화
         클래스를 제거하여 초기화합니다. 또한 inpw = "";를 사용하여 비밀번호 입력 변수 inpw를
         초기화합니다. 따라서, 비밀번호가 일치하지 않았을 때 이미 클릭한 숫자들이 초기화됩니다. */
+
+
     });
 
 
