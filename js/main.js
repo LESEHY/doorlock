121958ca16a36a3d1915df28ef2a49258c075994
LESEHY <tmdgus2496@gmail.com>
2023-02-27 18:44:24 +0900

    ss

--- js/main.js
+++ js/main.js
@@ -1,14 +1,30 @@
 $(() => {
-    console.log("로딩완");
+    console.log("정상작동 중");
 
     let num = $(".num");
-
-    num.on('click','li',function(){
+    let pw;
+    // 클릭했을 때 숫자인식
+    num.on('click', 'li', function () {
         const text = $(this).text();
         console.log(text);
-    })
-
+        pw += text;
+        console.log(pw);
+    });
     
+    // # 누를 시 
+    $(".num a").click(function (e) {
+        // # 눌러도 새로고침 X
+        e.preventDefault();
+     
+        if (pw === pw) {
+            alert("비밀번호를 변경하십시오.")
+        }
+        else{
+            alert("비밀번호가 맞지 않습니다.")
+        }
+
+    });
+
 
 
 }); // JQB
